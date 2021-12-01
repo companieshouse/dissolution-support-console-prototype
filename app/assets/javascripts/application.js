878915272d505f002fce06de3a1ee72ed41f1ec4
James Francis <jfrancis@companieshouse.gov.uk>
2021-12-01 11:04:44 +0000

    First version of prototype

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+/* global $ */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+})
+
+// Hide notification on the overview screen
+function dismiss () {
+  document.getElementById('notification').parentNode.style.display = 'none'
+};
+
+// RESULTS PER PAGE
+$('.table-per-page__input').change(function () {
+  $('#resultsPerPage').submit()
+  return false
+})
